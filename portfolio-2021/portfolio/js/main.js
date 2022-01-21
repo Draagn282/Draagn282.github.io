636db3e72f7fd971c498b0c3afcb4e307629e6e0
Draagn282 <daanpx@hotmail.com>
2022-01-21 15:09:36 +0100

    update 21-1-2022

--- portfolio-2021/portfolio/js/main.js
+++ portfolio-2021/portfolio/js/main.js
@@ -7,45 +7,30 @@ $(document).ready(function () {
       $(".header_main_div").removeClass("header_main_div_sticky");
       $(".header_butn").removeClass("header_butn_sticky");
     }
+
     if (this.scrollY < 500) {
       $(".scroll_up_butn").addClass("show");
     } else {
       $(".scroll_up_butn").removeClass("show");
     }
   });
-  $(".scroll-up-btn").click(function () {
-    $("html").animate({ scrollTop: 0 });
-    $("html").css("scrollBehavior", "auto");
-  });
-  $(".navbar .menu li a").click(function () {
-    $("html").css("scrollBehavior", "smooth");
-  });
-  $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
-  });
-  var typed = new Typed(".typing", {
-    strings: ["Gamer", "Developer", "Scout leiding", "Havist"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-  var typed = new Typed(".typing_2", {
-    strings: ["Believe", "Achieve", "Recieve"],
-    typeSpeed: 50,
-    backSpeed: 10,
-    loop: true,
-  });
-  $(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: { items: 1, nav: false },
-      600: { items: 2, nav: false },
-      1000: { items: 3, nav: false },
-    },
-  });
+});
+$(".header_butn_appear").click(function () {
+  $(".header_butn_appear").toggleClass("active1");
+  $(".header_main_div").toggleClass("active2");
+  $(".header_leftside_div").toggleClass("active2");
+  $(".header_rightside_div").toggleClass("active2");
+  $(".header_butn").toggleClass("active2");
+});
+var typed = new Typed(".typing", {
+  strings: ["Gamer", "Developer", "Scout leiding", "Havist"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+var typed = new Typed(".typing_2", {
+  strings: ["Believe", "Achieve", "Recieve"],
+  typeSpeed: 50,
+  backSpeed: 10,
+  loop: true,
 });
